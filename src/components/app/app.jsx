ec8ef8a52cd4bd710f3dcbed6bb2904a3dd682de
unknown <kristinamisenko9@gmail.com>
2022-09-09 00:18:29 +0300

    Настроена страница 404

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,22 +7,28 @@ import {
 import DetailedQuest from 'components/detailed-quest/detailed-quest';
 import Contacts from 'components/contacts/contacts';
 import Home from 'components/home/home';
+import NotFoundPage from 'components/not-found-page/not-found-page';
+
 import { appTheme } from './common';
 import * as S from './app.styled';
+
 
 const App = () => (
   <ThemeProvider theme={appTheme}>
     <S.GlobalStyle />
     <Router>
       <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
         <Route exact path="/quest">
           <DetailedQuest />
         </Route>
         <Route exact path="/contacts">
           <Contacts />
         </Route>
-        <Route path="/">
-          <Home />
+        <Route>
+          <NotFoundPage />
         </Route>
       </Switch>
     </Router>
