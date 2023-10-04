cd513ebf48def85bb2e92c2fae3252ac6e77f443
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2023-10-04 17:31:01 +0300

    service: добалвяет настройки для деплоя

--- src/components/common/footer/footer.tsx
+++ src/components/common/footer/footer.tsx
@@ -4,12 +4,6 @@ const Footer = (): JSX.Element => (
   <S.StyledFooter>
     <S.Socials>
       <S.SocialItem>
-        <S.SocialLink target="_blank" href="https://instagram.com">
-          <S.StyledInstagramIcon />
-          <S.HiddenText>Инстаграм</S.HiddenText>
-        </S.SocialLink>
-      </S.SocialItem>
-      <S.SocialItem>
         <S.SocialLink target="_blank" href="https://vk.com">
           <S.StyledVkIcon />
           <S.HiddenText>Вконтакте</S.HiddenText>
